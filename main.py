a04e14c898068895ed8a3054847c5fdfd7fa76e1
Dmitrii Bogaciov <dmitrii.bogaciov@gmail.com>
2023-03-17 07:06:17 +0100

    updated

--- main.py
+++ main.py
@@ -18,10 +18,61 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 pop = toolbox.population(n=IND_SIZE)
 print(pop)
 surface = 0.5
-top = 4
+top = 2
+lakes = 2
+flooding = 20
+smooth_surface = 20
+
+
 def evaluate(individual):
-    fitness = sum(individual)
+    fitness = 0
+    count_top = 0
+    count_lakes = 0
+    count_flooding = 0
+    count_smooth_surface = 0
+
+    # считаем количество гор, если гор не ровно нужному количеству, то непригодно
+    for i in range(-1, len(individual) - 1):
+        if individual[i] > individual[i - 1] and individual[i] > individual[i + 1]:
+            count_top += 1
+    if count_top == top:
+        fitness += 1
+    else:
+        fitness -= 1
+
+    # считаем количество озер, если озер не ровно нужному количеству, то непригодно
+    for i in range(-1, len(individual) - 1):
+        if individual[i] < surface:
+            if individual[i] < individual[i - 1] and individual[i] < individual[i + 1]:
+                count_top += 1
+    if count_lakes == lakes:
+        fitness += 1
+    else:
+        fitness -= 1
+
+    #  проверяем процент затопленности, если не ровно, то непригодно
+    for i in range(-1, len(individual) - 1):
+        if individual[i] < surface:
+            count_flooding += 1
+    new_flooding = count_flooding * 100 / len(individual)
+    if flooding - 10 <= new_flooding <= flooding + 10:
+        fitness += 1
+    else:
+        fitness -= 1
+
+    #  проверяем процент ровной поверхности, если не ровно, то непригодно
+    for i in range(-1, len(individual) - 1):
+        if individual[i] > surface:
+            if individual[i - 1] < surface:
+                if individual[i + 1] <= individual[i] <= individual[i + 1] + 0.1:
+                    count_smooth_surface += 1
+    new_smooth_surface = count_smooth_surface * 100 / len(individual)
+    if smooth_surface - 10 <= new_smooth_surface <= smooth_surface + 10:
+        fitness += 1
+    else:
+        fitness -= 1
     return fitness,
+
 
 
 toolbox.register("evaluate", evaluate)
@@ -30,17 +81,15 @@ toolbox.register("mate", tools.cxOnePoint)
 toolbox.register("mutate", tools.mutUniformInt, low=0, up=1, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
-NGEN = 10
-CXPB = 0.9
-MUTPB = 0.1
+NGEN = 100
+CXPB = 0.7
+MUTPB = 0.3
 
 s = tools.Statistics(key=lambda ind: ind.fitness.values)
 s.register("mean", np.mean)
 s.register("max", np.max)
 
 hof = tools.HallOfFame(1)  # pamatuje si 1 nejlepšího jedince za historii evoluce (i když zanikne)
-
-pop = toolbox.population(n=10)
 
 finalpop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=CXPB, mutpb=MUTPB, ngen=NGEN, stats=s, halloffame=hof)
 
@@ -52,16 +101,15 @@ print(hof)
 
 fig, ax = plt.subplots()
 
-ax.plot(range(1, len(hof[0])+1), hof[0], color='green', alpha=0.5)
+ax.plot(range(1, len(hof[0]) + 1), hof[0], color='green', alpha=0.5)
 
 ax.axhline(y=surface, color='blue', linestyle='-')
 
 # Закрашиваем область под линией до максимального значения
-ax.fill_between(range(1, len(hof[0])+1), hof[0], 0, color='green', alpha=0.5)
-ax.fill_between(range(1, len(hof[0])+1), 0, surface, color='blue', alpha=0.2)
+ax.fill_between(range(1, len(hof[0]) + 1), hof[0], 0, color='green', alpha=0.5)
+ax.fill_between(range(1, len(hof[0]) + 1), 0, surface, color='blue', alpha=0.2)
 
 ax.set_xlim(1, len(hof[0]))
 ax.set_ylim(0, 1)
 ax.legend()
 plt.show()
-
